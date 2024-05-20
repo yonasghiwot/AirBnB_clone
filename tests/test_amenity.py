8f09edb1ca937fa700d8158fac6dcac82e6483a2
yonasghiwot <yonihabeshaw@gmail.com>
2024-05-20 21:51:02 +0300

    tests

---
+++ tests/test_amenity.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Unittest for Amenity"""
+import os
+import time
+import unittest
+from datetime import datetime
+from models import storage
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """[test Amenity]"""
+
+    def test_init(self):
+        """[test amenity init blank test]"""
+        snapshot = datetime.now()
+        amenity1 = Amenity()
+        snapshot2 = datetime.now()
+
+        self.assertIsInstance(amenity1.id, str)
+        self.assertTrue(len(amenity1.id) > 0)
+        self.assertTrue('Amenity.' + amenity1.id in storage.all().keys())
+
+        self.assertIsInstance(amenity1.created_at, datetime)
+        self.assertLess(amenity1.created_at, snapshot2)
+        self.assertGreater(amenity1.created_at, snapshot)
+        
+        self.assertIsInstance(amenity1.updated_at, datetime)
+        self.assertLess(amenity1.updated_at, snapshot2)
+        self.assertGreater(amenity1.updated_at, snapshot)
+        
+        amenity1.save()
+        self.assertIsInstance(amenity1.updated_at, datetime)
+        self.assertGreater(amenity1.updated_at, snapshot)
+        self.assertGreater(amenity1.updated_at, snapshot2)
+        del amenity1
+
+    def test_init_dict(self):
+        """[test dict basemodel init]"""
+        test_dict = {'updated_at': datetime(1973, 11, 22, 12, 30, 00, 716921).isoformat('T')
+                     , 'id': 'z3854b62-93fa-fbbg-27de-630706f8313c', 'created_at': datetime(1973, 11, 22, 12, 30, 00, 716921).isoformat('T')}
+        amenity2 = Amenity(**test_dict)
+
+        self.assertIsInstance(amenity2.id, str)
+        self.assertTrue(len(amenity2.id) > 0)
+        self.assertTrue(amenity2.id == test_dict['id'])
+        
+        self.assertIsInstance(amenity2.created_at, datetime)
+        self.assertTrue(amenity2.created_at.isoformat('T') == test_dict['created_at'])
+        self.assertIsInstance(amenity2.updated_at, datetime)
+        self.assertTrue(amenity2.updated_at.isoformat('T') == test_dict['updated_at'])
+        am2.save()
+        self.assertGreater(amenity2.updated_at, amenity2.created_at)
+        del amenity2
+
+    def test_attribute(self):
+        """tets self atributes"""
+        amenity3 = Amenity()
+
+        self.assertTrue(hasattr(amenity3, "name"))
+        self.assertIsInstance(amenity3.name, str)

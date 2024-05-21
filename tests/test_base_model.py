3b04265b9b6ceff8b3f79782d3333742b8d7d6f9
yonasghiwot <yonihabeshaw@gmail.com>
2024-05-21 02:51:11 -0700

    test_base_model.py

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -24,17 +24,17 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(bm1.created_at, datetime)
         self.assertLess(bm1.created_at, snapshot2)
         self.assertGreater(bm1.created_at, snapshot)
-        
+
         self.assertIsInstance(bm1.updated_at, datetime)
         self.assertLess(bm1.updated_at, snapshot2)
         self.assertGreater(bm1.updated_at, snapshot)
-        
+
         bm1.save()
         self.assertIsInstance(bm1.updated_at, datetime)
         self.assertGreater(bm1.updated_at, snapshot)
         self.assertGreater(bm1.updated_at, snapshot2)
         del bm1
-        
+
     def test_init_dict(self):
         """test dict basemodel init"""
         test_dict = {'updated_at': datetime(1963, 11, 22, 12, 30, 00, 716921).isoformat('T')
@@ -44,7 +44,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(bm2.id, str)
         self.assertTrue(len(bm2.id) > 0)
         self.assertTrue(bm2.id == test_dict['id'])
-        
+
         self.assertIsInstance(bm2.created_at, datetime)
         self.assertTrue(bm2.created_at.isoformat('T') == test_dict['created_at'])
         self.assertIsInstance(bm2.updated_at, datetime)

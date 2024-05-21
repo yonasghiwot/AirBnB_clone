9888d2d454d1b8f8d100b2f8d6f4934ce82d69a2
yonasghiwot <yonihabeshaw@gmail.com>
2024-05-21 03:21:36 -0700

    betty

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module: BaseModel class 
+[Module: BaseModel class]
 """
 import models
 import uuid
@@ -24,8 +24,8 @@ class BaseModel():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.fromisoformat(value)
                     setattr(self, key, value)
-                return 
-             
+                return
+
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
